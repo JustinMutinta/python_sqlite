35ad04783d734d8208b3dee0116388376f3bbfa2
justinmutinta <justinmutinta@hotmail.com>
2022-04-10 23:36:09 -0500

    Loop to add Employees to database

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ c = conn.cursor()
 
 #c.execute("INSERT INTO employee VALUES ('Mary', 'Schafer', 50000)")
 
-c.execute("SELECT * FROM employee WHERE last='Schafer'")
+for num in range(2):
+    first_name = input("What is a good first name: ")
+    last_name = input("What is a good last name: ")
+    pay = input("Enter their pay: ")
+    c.execute(f"INSERT INTO employee VALUES ('{first_name}', '{last_name}', {pay})")
+    print()
+
+
+c.execute("SELECT * FROM employee")
 print(c.fetchall())
 conn.commit()
 
