0f1b7fb227db73f907cb30d5805f30f32df3db1b
justinmutinta <justinmutinta@hotmail.com>
2022-04-11 22:23:02 -0500

    Added functions to clean up usage.

--- main.py
+++ main.py
@@ -13,14 +13,25 @@ c = conn.cursor()
 
 #c.execute("INSERT INTO employee VALUES ('Mary', 'Schafer', 50000)")
 
-for num in range(2):
-    first_name = input("What is a good first name: ")
-    last_name = input("What is a good last name: ")
-    pay = input("Enter their pay: ")
+def database_input(first_name, last_name, pay):
     c.execute(f"INSERT INTO employee VALUES ('{first_name}', '{last_name}', {pay})")
-    print()
 
+def database_add_quantity(num):
+    for iteration in range(num):
+        first_name = input("Enter a good first name: ")
+        last_name = input("Enter a good last name: ")
+        pay = input("Enter their pay: ")
+        database_input(first_name, last_name, pay)
+        print()
 
+number_choice = int(input("How many would you like to add to the database? "))
+
+if number_choice < 0:
+    pass
+else:
+    database_add_quantity(number_choice)
+
+#c.execute("SELECT * FROM employee WHERE pay > 20000")
 c.execute("SELECT * FROM employee")
 print(c.fetchall())
 conn.commit()
