ca96958170d7d1486ff52499f203a6e0604fb61f
justinmutinta <justinmutinta@hotmail.com>
2022-04-12 22:28:13 -0500

    Added remove user and cleaned up output function

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import sqlite3
 
 conn = sqlite3.connect('employee.db')
-
 c = conn.cursor()
 
 
-#c.execute("""CREATE TABLE employee (
- #           first text,
-  #          last text,
-   #         pay integer
-    #        )""")
+def create_new_table():
+    c.execute("""CREATE TABLE employee (
+                first text,
+                last text,
+                pay integer
+                )""")
 
-#c.execute("INSERT INTO employee VALUES ('Mary', 'Schafer', 50000)")
 
 def database_input(first_name, last_name, pay):
     c.execute(f"INSERT INTO employee VALUES ('{first_name}', '{last_name}', {pay})")
+
 
 def database_add_quantity(num):
     for iteration in range(num):
@@ -24,16 +24,43 @@ def database_add_quantity(num):
         database_input(first_name, last_name, pay)
         print()
 
+
+def database_remove_user(first_name, last_name):
+    c.execute(f"DELETE FROM employee WHERE first='{first_name}' AND last='{last_name}'")
+
+
+def output_database_contents():
+    c.execute("SELECT * FROM employee")
+    print(c.fetchall())
+
+
+output_database_contents()
+
+
 number_choice = int(input("How many would you like to add to the database? "))
 
-if number_choice < 0:
-    pass
+
+if number_choice < 1:
+    print("No entries to be added")
 else:
     database_add_quantity(number_choice)
+    output_database_contents()
 
-#c.execute("SELECT * FROM employee WHERE pay > 20000")
-c.execute("SELECT * FROM employee")
-print(c.fetchall())
+
+ask_to_delete = input("Would you like to delete an employee: y/n? ...")
+
+
+if ask_to_delete == "y":
+    first_name = input("What is their first name? ")
+    last_name = input("What is their last name? ")
+    database_remove_user(first_name, last_name)
+    output_database_contents()
+elif ask_to_delete == "n":
+    print("No entries to be deleted")
+else:
+    print("I don't know what that is")
+    pass
+
+
 conn.commit()
-
 conn.close()
